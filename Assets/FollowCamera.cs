691dfe6ad3fc8bcc0f2678d61247ca423b984264
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-02-26 16:34:00 +0100

    Camera fix

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject followPoint;
+
+    private void Update()
+    {
+        transform.position = followPoint.transform.position;
+    }
+}
