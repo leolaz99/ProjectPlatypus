d6fcba480acef7705ef2583399a443e3cd9ca900
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-02-27 00:24:26 +0100

    Cose

--- Assets/Scenes/Gameplay/Scripts/PlayerLife.cs
+++ Assets/Scenes/Gameplay/Scripts/PlayerLife.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
-using DG.Tweening;
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
     public uint life;
     public static PlayerLife instance;
+    private bool isInvincible = false;
+
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" && isInvincible == false)
+        {
             life--;
+            StartCoroutine("InvulnerableTime");
+        }
+
     }
 
     void Awake()
@@ -22,5 +29,12 @@ public class PlayerLife : MonoBehaviour
     {
         if (life <= 0)
             transform.DOMoveY(-2, 10);
+    }
+
+    IEnumerator InvulnerableTime()
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(3);
+        isInvincible = false;
     }
 }
