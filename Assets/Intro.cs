8459decaf89ff691007297834643915fcb1cfcee
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-02-27 00:01:46 +0100

    Intro

---
+++ Assets/Intro.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField]
+    private Image _blackPanel;
+    [SerializeField]
+    private GameObject _blackObject;
+    [SerializeField]
+    private GameObject introContinueButton;
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private GameObject textObject;
+
+    public void RemoveBlackPanel()
+    {
+        FadeOut();
+        introContinueButton.SetActive(false);
+        Invoke("BlackPanelDisappears", 1f);
+    }
+    private void BlackPanelDisappears()
+    {
+        //attiva il gameobject del pannello nero
+        _blackObject.SetActive(false);
+        textObject.SetActive(false);
+    }
+    private void FadeOut()
+    {
+        //cambia l'alpha del pannello nero a 0(totalmente trasparente) in X secondi(secondo paramentro)
+        _blackPanel.CrossFadeAlpha(0, 1f, false);
+        text.CrossFadeAlpha(0, 1f, false);
+    }
+}
--- Assets/Scenes/Menu/Scripts/SceneLoader.cs
+++ Assets/Scenes/Menu/Scripts/SceneLoader.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject settings;
     [SerializeField] GameObject credits;
+
+    [SerializeField]
+    private Image _blackPanel;
+    [SerializeField]
+    private GameObject _blackObject;
+
 
     public void GoToSettings()
     {
@@ -26,6 +33,13 @@ public class SceneLoader : MonoBehaviour
         mainMenu.gameObject.SetActive(true);
     }
 
+    public void PlayButtonClicked()
+    {
+        BlackPanelAppears();
+        FadeIn();
+        Invoke("GoToGame", 0.5f);
+    }
+
     public void GoToGame()
     {
         SceneManager.LoadScene("Gameplay");
@@ -37,4 +51,20 @@ public class SceneLoader : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+    private void FadeIn()
+    {
+        //cambia l'alpha del pannello nero a 1(totalmente nero) in X secondi(secondo paramentro) dopo averla impostata a 0
+        _blackPanel.canvasRenderer.SetAlpha(0f);
+        _blackPanel.CrossFadeAlpha(1, 0.4f, false);
+    }
+
+    private void BlackPanelAppears()
+    {
+        //disattiva il gameobject del pannello nero
+
+        _blackObject.SetActive(true);
+    }
+
 }
