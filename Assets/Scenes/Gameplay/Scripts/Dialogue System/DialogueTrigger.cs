6533155f190c91899cd33fc7d2785bd566df084e
leolaz99 <leolaz99@gmail.com>
2021-02-27 19:40:11 +0100

    added tutorial system

--- Assets/Scenes/Gameplay/Scripts/Dialogue System/DialogueTrigger.cs
+++ Assets/Scenes/Gameplay/Scripts/Dialogue System/DialogueTrigger.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
     public PowerUpManager powerUpManager;
+    [TextArea(3, 10)]
+    public string tutorialSentence;
+    public Text tutorialText;
+    public float tutorialTime;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -11,14 +17,23 @@ public class DialogueTrigger : MonoBehaviour
             TriggerDialogue();
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(ShowTutorial());
+    }
+
+    public IEnumerator ShowTutorial()
+    {
+        tutorialText.text = tutorialSentence;
+        tutorialText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(tutorialTime);
+        tutorialText.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+    
     public void TriggerDialogue()
     {
         powerUpManager.powerUpCounter++;
         DialogueManager.instance.StartDialogue(dialogue);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        this.gameObject.SetActive(false);
     }
 }
