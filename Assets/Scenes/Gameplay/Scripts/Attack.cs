82dd05a01733812d68083d039c30579f5a8a3ad4
leolaz99 <leolaz99@gmail.com>
2021-02-28 18:58:01 +0100

    added menu

--- Assets/Scenes/Gameplay/Scripts/Attack.cs
+++ Assets/Scenes/Gameplay/Scripts/Attack.cs
@@ -4,12 +4,14 @@ public class Attack : MonoBehaviour
 {
     [SerializeField] KeyCode attackKey;
     [SerializeField] GameObject attackCollider;
+    public Animator anim;
 
     void Update()
     {
         if (Input.GetKeyDown(attackKey))
         {
             attackCollider.SetActive(true);
+            anim.SetTrigger("IsAttack");
         }
     }
 }
